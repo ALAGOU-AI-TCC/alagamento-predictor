1c0f980bb2449da213e278411fabd7bd5e1d3484
Gaalfp <Gaalfp>
2025-08-08 00:31:07 +0000

    Análise do DataFrame e Treinamento do Modelo

--- src/processar_data.py
+++ src/processar_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('../data/raw/registros-base-17-07-2025.csv')
+df = pd.read_csv('../data/raw/registros-base-07-08-2025.csv')
 df.replace(['NULL', 'null', ''], pd.NA, inplace=True)
 df = df.drop(columns=['id'])
 df = df.dropna(subset=[
@@ -15,9 +15,22 @@ df = df.dropna(subset=[
     'historico_alagamento',
     'precipitacao_acumulada'
 ])
+colunas_numericas = [
+    'temperatura', 'umidade', 'pressao', 'velocidade_vento',
+    'precipitacao_chuva', 'tempo_chuva', 'ponto_orvalho',
+    'precipitacao_acumulada'
+]
+df[colunas_numericas] = df[colunas_numericas].apply(pd.to_numeric, errors='coerce')
+
+df['historico_alagamento'] = df['historico_alagamento'].astype(int)
 
 df['intensidade_chuva'] = df['intensidade_chuva'].str.lower()
 
+df['bairro'] = df['bairro'].str.lower().str.strip()
+
 df['data_hora'] = pd.to_datetime(df['data_hora'].str.replace('T', ' '))
+
+inconsistentes = df[(df['tempo_chuva'] == 0) & (df['precipitacao_chuva'] > 0)]
+print(f"{len(inconsistentes)} registros inconsistentes encontrados.")
 
 df.to_csv('../data/processed/dados_processados.csv', index=False)
